d3c36b6eb037ed2ab46c15672e63f94832c3690f
Grace J Adamiec and Michael Bradt <michael@>
2018-08-07 14:44:58 -0700

    have working fade toggle for first four flashcards

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,19 @@ $(document).ready(function(){
     $(".clickable1 .term1").fadeToggle();
     $(".clickable1 .def1").fadeToggle();
   });
+
+  $(".clickable2").click(function(){
+    $(".clickable2 .term2").fadeToggle();
+    $(".clickable2 .def2").fadeToggle();
+  });
+
+  $(".clickable3").click(function(){
+    $(".clickable3 .term3").fadeToggle();
+    $(".clickable3 .def3").fadeToggle();
+  })
+
+  $(".clickable4").click(function(){
+    $(".clickable4 .term4").fadeToggle();
+    $(".clickable4 .def4").fadeToggle();
+  })
 });
